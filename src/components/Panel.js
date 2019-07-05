1937169cf2c162c3f5370bacca59999a7d31a9dc
DebanjanDey-Kreeti <debanjan.dey@kreeti.com>
2019-07-05 19:18:40 +0530

    Our 2nd commit

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -7,12 +7,12 @@ const Panel = (props) => {
       <div className="d-flex flex-row">
         <div className="col-md-4 mx-auto">
           <div className="panel">
+            <label className="panel-switch-text" htmlFor="date-switch">
+              <i className="fa fa-calendar"></i>
+            </label>
             <label id="date-switch" className="switch">
               <input type="checkbox" checked= {props.dateOn} onChange={props.toggleDate} />
               <span className="slider round"></span>
-            </label>
-            <label className="panel-switch-text" htmlFor="date-switch">
-              <i className="fa fa-calendar"></i>
             </label>
           </div>
         </div>
